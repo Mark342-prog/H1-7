e59053b7ebc6b27e9421b7534f51e7991d2a8065
Raul <san21401@uvg.edu.gt>
2022-01-19 14:23:16 -0600

    Primeras tres opciones del main
    Se puso las funcionalidades de las primeras tres opciones del radio

--- radio_main.java
+++ radio_main.java
@@ -14,31 +14,38 @@ public class radio_main{
          * Generacion de scanner y de objeto
          */
         Scanner sc = new Scanner(System.in);
+        Scanner sc1= new Scanner(System.in);
         radio_cls Radio=new radio_cls();
         
             /**
             * Variable para opciones de menu
             */
         int op=0;
+        boolean on = false;
+        boolean frequency = false; //falso es fm y am es true
+        Integer st_am = 530; 
+        double st_fm = 87.9;
+        Integer pos;
             /**
              * Ciclo while para ejectutar las opciones
             */
-        while (op !=9)
+        while (op !=7)
         {
             System.out.println("\n Bienvenido al simulador del radio");
             System.out.println("\n Elija las opciones que considere que necesite");
             System.out.println("1.Encender el radio ");
             System.out.println("2.Cambiar de AM a FM a AM");
-            System.out.println("3.Avanzar en el dial de las emisoras.");
-            System.out.println("4. Guardarr una emisora en uno de los 12 botones");
+            System.out.println("3.Avanzar o regresar en el dial de las emisoras.");
+            System.out.println("4. Guardar una emisora en uno de los 12 botones");
             System.out.println("5. Seleccionar la emisora opuesta de un botón");
             System.out.println("6. Apagar el radio");
             System.out.println("7. Salir");
+           
             /**
              *  Lectura de opcion ingresada 
            */
             op=sc.nextInt();
-        }
+        
         switch (op)
         {
             case 1:
@@ -46,16 +53,34 @@ public class radio_main{
             System.out.println("Acaba de encender el radio");
             break;
             case 2:
-
+            Radio.switchAMFM();
+            System.out.println("Ha cambiado de AM a FM");
             break;
             case 3:
+            System.out.println("1. Ver emisora actual");
+            System.out.println("2. Avanzar la emisora");
+            System.out.println("3. Regresar a la emisora");
+            int op1=0;
+            op1=sc1.nextInt();
+                if(op1==1)
+                {
+                    Radio.getStation();
+                }
+                else if(op1==2)
+                {
+                    Radio.nextStation(frequency);
+                }
+                else if (op1==3)
+                {
+                    Radio.prevStation(frequency);
+                }
 
             break;
             case 4:
-
+            
             break;
             case 5:
-
+            //Seleccionar de AM a FM
             break;
             case 6:
             Radio.turnOnOff();
@@ -66,9 +91,7 @@ public class radio_main{
             break;
 
         }
-        
-        sc.close(); 
-
     }
-   
+    sc.close(); 
+}
 }
